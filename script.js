04102a45a388e5dcc94872b59f099362ec8ad299
yudasetyo <yudasetyo07@gmail.com>
2023-12-06 16:30:22 +0700

    feat: Switching the active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const plater0E = document.querySelector('.player--0');
+const plater1E = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,7 +33,13 @@ btnRoll.addEventListener('click', function () {
   // 3. Check rolled result
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    plater0E.classList.toggle('player--active');
+    plater1E.classList.toggle('player--active');
   }
 });
